385d70ff94b1f4cd6c7990caa4da5f612b0a0808
Nirlley Costa <nirlleycosta@gmail.com>
2023-01-24 19:48:51 -0300

    commit inicial

---
+++ src/CalculadoraDeSalario.java
@@ -0,0 +1,9 @@
+public class CalculadoraDeSalario {
+
+    public double calculaSalario(Funcionario funcionario) {
+        if (funcionario.getSalario() > 3000) {
+            return funcionario.getSalario() * 0.8;
+        }
+        return funcionario.getSalario() * 0.9;
+    }
+}
